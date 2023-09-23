c88ccdacba112e8c959e426834ef4ca7b06d16c5
Daniel Castro <danielcastt@gmail.com>
2023-09-23 14:07:09 -0300

    Meu Primeiro commit de ruby

---
+++ ruby-desafio01/desafio01.rb
@@ -0,0 +1,9 @@
+puts"Sistema Biblioteca"
+puts"------------------"
+puts"Digite seu nome: "
+nome = gets.chomp
+puts"Digite seu sobrenome: "
+sobrenome = gets.chomp
+puts"Digite sua idade: "
+idade = gets.chomp
+puts"Bem-vindo, #{nome} #{sobrenome}, de #{idade} anos!"
